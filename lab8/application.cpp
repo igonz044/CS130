a961dac59b0bbb552abe2457e0a33ede994d00eb
Itzel <44009297+igonz044@users.noreply.github.com>
2019-05-09 12:49:01 -0700

    Add files via upload

--- lab8/application.cpp
+++ lab8/application.cpp
@@ -19,13 +19,86 @@
 using namespace std;
 enum { NONE, AMBIENT, DIFFUSE, SPECULAR, NUM_MODES };
 
+
+vec3 interpolate(vec3 a, vec3 b, float u)
+{
+    return a * (1 - u) + b * u;
+}
+
+vec3 particleColor(float d)
+{
+  if(d < .1){return vec3(1, 1, 0);} //returns yellow
+  else if(d < 1.5)
+  {
+    return interpolate(vec3(1, 1, 0), vec3(1, 0, 0), (d - .1) / 1.4);
+  }//yellow to red
+  else if(d < 2) { return vec3(1, 0, 0); }//red
+  else if(d < 3)
+  {
+    return interpolate(vec3(1, 0, 0), vec3(.5, .5, .5), (d - 2) / 1);
+  }//red to grey
+  else { return vec3(.5, .5, .5); }//grey
+}
+
+struct Particle
+{
+    float mass;
+    float d = 0.0;
+    vec3 position;
+    vec3 velocity;
+    vec3 force;
+    vec3 color;
+
+    void euler_step(float h)
+    {
+      d = d + h;
+      velocity += (h / mass) * force;
+      position += h * velocity;
+      color = particleColor(d);
+    }
+
+    void reset_forces(){ force = vec3(0, 0, 0); }
+
+    void handle_collision(float damping, float coeff_resititution)
+    {
+      if(position[1] < 0)
+      {
+        position[1] = 0;
+        if(velocity[1] < 0)
+        {
+          velocity[1] = -1.0 * coeff_resititution * velocity[1];
+          velocity[2] = damping * velocity[2];
+          velocity[1] = damping * velocity[1];
+        }
+      }
+    }
+};
+
+vector<Particle> particles;
+float random(float k, float l)
+{
+  return k + static_cast<float> (rand())/( static_cast<float> (RAND_MAX/(1-k)));
+}
+void add_particles(int n)
+{
+  for(int i = 0; i < n; i++)
+  {
+    Particle p;
+    p.mass = 1;
+    p.d = 0;
+    p.position = vec3(random(-0.2, 0.2), -.05, random(-1, -.2));
+    p.velocity = vec3(10 * p.position[0], random(1, 10), 10 * p.position[2]);
+    p.color = vec3(1, 1, 0);
+    particles.push_back(p);
+  }
+}
 void draw_grid(int dim);
 void draw_obj(obj *o, const gl_image_texture_map& textures);
 
 void set_pixel(int x, int y, float col[3])
 {
     // write a 1x1 block of pixels of color col to framebuffer
-    // coordinates (x, y)
+    //coordinates (x, y)
     //glRasterPos2i(x, y);
     //glDrawPixels(1, 1, GL_RGB, GL_FLOAT, col);
 
@@ -37,13 +110,9 @@ void set_pixel(int x, int y, float col[3])
 }
 
 application::application()
-    : raytrace(false), rendmode(SPECULAR), paused(false), sim_t(0.0),draw_volcano(true),h(0.015)
-{
-}
+    : raytrace(false), rendmode(SPECULAR), paused(false), sim_t(0.0),draw_volcano(true),h(0.015){}
 
-application::~application()
-{
-}
+application::~application(){}
 
 // triggered once after the OpenGL context is initialized
 void application::init_event()
@@ -91,8 +160,7 @@ void application::init_event()
     // loads up all the textures referenced by the .mtl file
     const std::map<std::string, obj::material>& mats = o.get_materials();
     for (std::map<std::string, obj::material>::const_iterator i = mats.begin();
-        i != mats.end(); ++i
-        )
+        i != mats.end(); ++i)
     {
         if (!i->second.map_kd.empty()) {
             string filename = i->second.map_kd;
@@ -108,6 +176,7 @@ void application::init_event()
             }
         }
     }
+    add_particles(10);
 }
 
 // triggered each time the application needs to redraw
@@ -118,30 +187,37 @@ void application::draw_event()
     const GLfloat light_pos1[] = { 0.0, 10.0, 0.0, 1 };
     glLightfv(GL_LIGHT1, GL_POSITION, light_pos1);
 
-    if (!paused) {
-        //
+    if (!paused)
+    {
         //ADD NEW PARTICLES
-        //
-        //
-        // SIMULATE YOUR PARTICLE HERE.
-        //
-        //
-        //
-        // UPDATE THE COLOR OF THE PARTICLE DYNAMICALLY
-        //
+        //SIMULATE YOUR PARTICLE HERE.
+        //UPDATE THE COLOR OF THE PARTICLE DYNAMICALLY
+        add_particles(80);
+        for(auto& p : particles)
+        {
+          p.reset_forces();
+          p.force = p.force + vec3(0, -1.0 * p.mass * 9.81, 0);
+          p.euler_step(h);
+          p.handle_collision(.5, .5);
+        }
     }
 
     glLineWidth(2.0);
     glEnable(GL_COLOR_MATERIAL);
     glBegin(GL_LINES);
-        //
-        //
+
         // DRAW YOUR PARTICLE USING GL_LINES HERE
-        //
-        // glVertex3f(...) endpoint 1
-        // glVertex3f(...) endpoint 2
-        //
-        //
+   for(Particle p : particles)
+   {
+     glColor3f(p.color[0], p.color[1], p.color[2]);
+     glVertex3f(p.position[0], p.position[1], p.position[2]);
+
+
+     vec3 endPosition;
+     endPosition = p.position + static_cast <float>(.04) * p.velocity;
+     glVertex3f(endPosition[0], endPosition[1], endPosition[2]);
+   }
+
     glEnd();
 
     // draw the volcano
---
+++ lab8/gl_texture.d
@@ -0,0 +1,2 @@
+gl_texture.d gl_texture.o: gl_texture.cpp gl_texture.h /usr/local/include/png.h \
+  /usr/local/include/pnglibconf.h /usr/local/include/pngconf.h
